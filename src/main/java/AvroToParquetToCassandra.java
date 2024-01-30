c1bf5dcad74e211b9fec14270f10ca5456ce4373
Vishv Patel <158057613+vishv-97@users.noreply.github.com>
2024-01-30 15:41:41 +0530

    Add files via upload

---
+++ src/main/java/AvroToParquetToCassandra.java
@@ -0,0 +1,48 @@
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+public class AvroToParquetToCassandra {
+    public static void main(String[] args) {
+        // Set up Spark configuration
+        SparkConf conf = new SparkConf().setAppName("AvroToParquetToCassandra").setMaster("local[]");
+
+        // Set AWS credentials directly in the code (not recommended for security reasons)
+        conf.set("spark.hadoop.fs.s3a.access.key", "AKIATG3UFWTEYFSIDVJZ");
+        conf.set("spark.hadoop.fs.s3a.secret.key", "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");
+
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        SparkSession spark = SparkSession.builder().appName("AvroToParquetToCassandra").getOrCreate();
+
+        // Specify S3 bucket path for input Avro file
+        String inputS3Path = "s3a://demo-rsoft-avro-to-parquet/avro-data/sample1.avro";
+
+        // Read Avro file into a DataFrame
+        Dataset<Row> avroDataFrame = spark.read().format("avro").load(inputS3Path);
+
+        // Specify S3 bucket path for output Parquet file
+        String outputS3Path = "s3a://demo-rsoft-avro-to-parquet/avro-data/parquet-data/sample1.parquet";
+
+        // Write DataFrame to Parquet file in S3
+        avroDataFrame.write().parquet(outputS3Path);
+
+        // Specify Cassandra connection parameters
+        String cassandraHost = "127.0.0.1";
+        String cassandraKeyspace = "avro_to_parquet";
+        String cassandraTable = "sample";
+
+        // Write DataFrame to Cassandra
+        avroDataFrame.write()
+                .format("org.apache.spark.sql.cassandra")
+                .option("spark.cassandra.connection.host", cassandraHost)
+                .option("keyspace", cassandraKeyspace)
+                .option("table", cassandraTable)
+                .mode("append")
+                .save();
+
+        // Stop Spark context
+        spark.stop();
+    }
+}
